f9b3480d63968d5c5dad4e058d8214a5f4e68d44
JasurAS <cesurehmedov594@gmail.com>
2024-09-12 14:58:07 +0400

    changes

--- src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -2,15 +2,21 @@ package com.learnkafka.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.domain.LibraryEventType;
 import com.learnkafka.producer.LibraryEventsProducer;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @Slf4j
 @RestController
@@ -21,10 +27,29 @@ public class LibraryEventsController {
     private final LibraryEventsProducer libraryEventsProducer;
 
     @PostMapping("/library-event")
-    @ResponseStatus(HttpStatus.CREATED)
-    public LibraryEvent postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
-        log.info("libraryEvent triggered, {}", libraryEvent);
+    public ResponseEntity<LibraryEvent> createLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException {
+        log.info("createLibraryEvent triggered, {}", libraryEvent);
         libraryEventsProducer.sendLibraryEvent(libraryEvent);
-        return libraryEvent;
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+    @PutMapping("/library-event")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> updateLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException {
+        log.info("updateLibraryEvent triggered, {}", libraryEvent);
+        ResponseEntity<String> BAD_REQUEST = validateLibraryEvent(libraryEvent);
+        if (BAD_REQUEST != null) return BAD_REQUEST;
+        libraryEventsProducer.sendLibraryEvent(libraryEvent);
+        return ResponseEntity.status(HttpStatus.OK).body(libraryEvent);
+    }
+
+    private static ResponseEntity<String> validateLibraryEvent(LibraryEvent libraryEvent) {
+        if (Objects.isNull(libraryEvent.libraryEventId())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please pass the libraryEventId");
+        }
+        if (!libraryEvent.libraryEventType().equals(LibraryEventType.UPDATE)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Only UPDATE type is supported");
+        }
+        return null;
     }
 }
