c9b7b326657bdbf5dbb8b678b3d55f98223cda61
JasurAS <cesurehmedov594@gmail.com>
2024-09-12 12:54:32 +0400

    changes

--- src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -1,19 +1,30 @@
 package com.learnkafka.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.producer.LibraryEventsProducer;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/v1")
-@Slf4j
+@RequiredArgsConstructor
 public class LibraryEventsController {
+
+    private final LibraryEventsProducer libraryEventsProducer;
 
     @PostMapping("/library-event")
     @ResponseStatus(HttpStatus.CREATED)
-    public LibraryEvent postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
+    public LibraryEvent postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
         log.info("libraryEvent triggered, {}", libraryEvent);
+        libraryEventsProducer.sendLibraryEvent(libraryEvent);
         return libraryEvent;
     }
 }
--- src/main/java/com/learnkafka/producer/LibraryEventsProducer.java
+++ src/main/java/com/learnkafka/producer/LibraryEventsProducer.java
@@ -1,13 +1,58 @@
 package com.learnkafka.producer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.learnkafka.domain.LibraryEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class LibraryEventsProducer {
 
     private final KafkaTemplate<Integer, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
+    @Value("${spring.kafka.topic}")
+    public String topic;
+
+    public CompletableFuture<SendResult<Integer, String>> sendLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
+        var key = libraryEvent.libraryEventId();
+        var value = objectMapper.writeValueAsString(libraryEvent);
+        var completableFuture = kafkaTemplate.send(topic, key, value);
+
+        return completableFuture
+                .whenComplete((sendResult, throwable) -> {
+                    if (throwable != null) {
+                        handleFailure(throwable);
+                    } else {
+                        handleSuccess(key, value, sendResult);
+                    }
+                });
+    }
+
+    private ProducerRecord<Integer, String> buildProducerRecord(Integer key, String value) {
+        List<Header> recordHeaders = List.of(new RecordHeader("event-source", "scanner".getBytes()));
+        return new ProducerRecord<>(topic, null, key, value, recordHeaders);
+    }
+
+    private void handleSuccess(Integer key, String value, SendResult<Integer, String> sendResult) {
+        log.info("Message Sent Successfully for the key: {} and the value: {}, partition is {}",
+                key, value, sendResult.getRecordMetadata().partition());
+    }
+
+    private void handleFailure(Throwable ex) {
+        log.error("Error sending the message and the exception is {}", ex.getMessage(), ex);
+    }
 }
