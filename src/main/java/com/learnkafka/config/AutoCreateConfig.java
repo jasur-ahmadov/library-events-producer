4e0fd23655bbb4dc7b8752449f3b98c04a1ffdc9
JasurAS <cesurehmedov594@gmail.com>
2024-09-12 15:34:36 +0400

    changes

--- src/main/java/com/learnkafka/config/AutoCreateConfig.java
+++ src/main/java/com/learnkafka/config/AutoCreateConfig.java
@@ -2,7 +2,8 @@ package com.learnkafka.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
